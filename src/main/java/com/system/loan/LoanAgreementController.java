a214601f7403ef8f6c7052aaa5584563fbfa376b
korbkhout <korbkhout@gmail.com>
2015-09-25 13:33:49 +0700

    kk

---
+++ src/main/java/com/system/loan/LoanAgreementController.java
@@ -0,0 +1,29 @@
+package com.system.loan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/LoanAgreement")
+public class LoanAgreementController {
+
+		@RequestMapping(value="/newLoanAgreement")
+	public String newLoanAgreement(){
+		return "loan_agreement";
+	}
+	
+		
+		@RequestMapping(value="/schedulepay")
+		public String SchedulePayment(){
+			return "customer";
+		}
+		@RequestMapping(value="/send_email")
+		public String customerOfficer(){
+			return "send_email";
+		}
+		@RequestMapping(value="/mail")
+		public String loanIncome(){
+			return "mail";
+		}
+		 
+}
