5d41f7cffa7a34293ccc3ad56667e6ad7bfdf488
kreysophannatt <krysophannatt@gmail.com>
2015-11-06 13:35:29 +0700

    sss

--- src/main/java/com/system/loan/dao/CustomerOfficerDaoImp.java
+++ src/main/java/com/system/loan/dao/CustomerOfficerDaoImp.java
@@ -10,6 +10,9 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -24,13 +27,16 @@ public class CustomerOfficerDaoImp {
 	public static SessionFactory factory = null;
 
 	public CustomerOfficerDaoImp() {
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure() // configures settings from hibernate.cfg.xml
+				.build();
 		try {
-			factory = new Configuration().configure().buildSessionFactory();
-		} catch (HibernateException e) {
-			System.out.println(e.toString());
-			e.printStackTrace();
-			if (e.getCause() != null)
-				System.out.println(e.getCause().getMessage());
+			factory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+		}
+		catch (Exception e) {
+			// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+			// so destroy it manually.
+			StandardServiceRegistryBuilder.destroy( registry );
 		}
 	}
 
