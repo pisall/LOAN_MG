0800332ee63dc70456d59d5419e44f861aeb73df
kreysophannatt <krysophannatt@gmail.com>
2015-11-06 14:22:21 +0700

    fsdfs

--- src/main/java/com/system/loan/TransactonController.java
+++ src/main/java/com/system/loan/TransactonController.java
@@ -1,13 +1,11 @@
 package com.system.loan;
 
-import java.io.Serializable; 
-import java.util.Map;
+import java.io.Serializable;
 
-import org.springframework.stereotype.Controller; 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod; 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 
