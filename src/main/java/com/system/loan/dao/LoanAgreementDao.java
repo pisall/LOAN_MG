49daba31370902144d7752d4f8a4e05befeb6482
kreysophannatt <krysophannatt@gmail.com>
2015-11-06 13:34:56 +0700

    sss

--- src/main/java/com/system/loan/dao/LoanAgreementDao.java
+++ src/main/java/com/system/loan/dao/LoanAgreementDao.java
@@ -7,6 +7,9 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 import com.system.loan.dto.AcountInfoDto;
@@ -19,20 +22,22 @@ public class LoanAgreementDao implements LoanAgreement{
 	public static SessionFactory factory = null;
 	
 	public LoanAgreementDao(){
-		try{
-			factory = new Configuration().configure().buildSessionFactory(); 
-		}catch(HibernateException e){
-			System.out.println(e.toString());
-			e.printStackTrace();
-			if(e.getCause()!=null){
-				System.out.println(e.getCause().getMessage());
-			}
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure() // configures settings from hibernate.cfg.xml
+				.build();
+		try {
+			factory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+		}
+		catch (Exception e) {
+			// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+			// so destroy it manually.
+			StandardServiceRegistryBuilder.destroy( registry );
 		}
 	}
 
 	@Override
 	public boolean InsertNewCustomer( LoanAgreementDto loanAgreDto) { 
-		Session session = factory.openSession(); 
+		Session session = factory.getCurrentSession();
 		Transaction transection = null;  
 		CoDto codto =null;
 		LoanAgreementDto loancus=null; 
@@ -59,8 +64,6 @@ public class LoanAgreementDao implements LoanAgreement{
 			if(transection!=null) transection.rollback();
 			e.printStackTrace();
 			return false;
-		}finally{
-			session.close();
 		}
 		return true;
 	}
