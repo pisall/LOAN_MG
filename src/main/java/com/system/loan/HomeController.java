3c20153bdd9a9407bbae598dbd6b2ae6ff32a41b
VIRAK_PC <VIRAK_PC@VIRAK_PC>
2015-11-19 14:25:21 +0700

    12313

---
+++ src/main/java/com/system/loan/HomeController.java
@@ -0,0 +1,187 @@
+package com.system.loan;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.naming.directory.DirContext;
+import javax.security.sasl.AuthenticationException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.xml.ws.RequestWrapper;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.ldap.core.ContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sun.net.httpserver.HttpContext;
+import com.system.loan.dao.LdapUserDetailsManager;
+import com.system.loan.dao.myUserDetailsManager;
+import com.system.loan.dao.login.LOGIN_DAO_001_IMP;
+import com.system.loan.dto.login.in.login_0001_in;
+import com.system.loan.dto.session.USER_SESSION;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	@Inject 
+	LOGIN_DAO_001_IMP loginDao;
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value ={"/","home"}, method = RequestMethod.GET)
+	public String home(HttpServletRequest req) {
+		HttpSession session=req.getSession();
+		USER_SESSION user=(USER_SESSION)session.getAttribute("USER_SESSION");
+		String test=user.getCoNm();
+		System.out.println("session naem="+test); 
+		return "home"; 
+	}
+	
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String adminPage(ModelMap model) {
+		model.addAttribute("user", getPrincipal());
+		return "admin";
+	}
+	
+	@RequestMapping(value = "/db", method = RequestMethod.GET)
+	public String dbaPage(ModelMap model) {
+		model.addAttribute("user", getPrincipal());
+		return "dba";
+	}
+
+	@RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
+	public String accessDeniedPage(ModelMap model) {
+		model.addAttribute("user", getPrincipal());
+		return "accessDenied";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String loginPage(HttpServletRequest req,HttpServletResponse res) {
+		req.setAttribute("username", "prak");
+		return "login";
+	}
+
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null){    
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/login?logout";
+	}
+	
+	private String getPrincipal(){
+		String userName = null;
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal instanceof UserDetails) {
+			userName = ((UserDetails)principal).getUsername();
+			System.out.println("user name="+userName);
+		} else {
+			userName = principal.toString();
+		}
+		return userName;
+	}
+	
+	@RequestMapping(value="/changePassword",method=RequestMethod.POST)
+	@ResponseBody
+	public HashMap<String , Object> changePassword(@RequestBody login_0001_in input){
+		
+		HashMap<String, Object> result=new HashMap<>();
+//		Object principal=SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//		String userName=principal.toString();
+//		if(principal instanceof UserDetails){
+//			userName=((UserDetails)principal).getUsername();
+//			System.out.println("userName="+userName+ ","+((UserDetails)principal).getPassword());
+//			
+//		}
+//		
+//		HashMap<String, Object> changeResult=loginDao.changePassword(input, userName);
+//		if((boolean)changeResult.get("ERROR")){
+//			result.put("ERROR", true);
+//		}else{
+//			result.put("ERROR", false);
+//		}
+//		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//		AuthenticationManager au=new 
+//		
+//		String name = SecurityContextHolder.getContext().getAuthentication().toString();
+//
+//		try {
+//			auth.isAuthenticated();
+//		    // Update password here with your dao
+//		} catch (AuthenticationException e) {
+//		    // Old password was wrong
+//		}
+		
+		//UserDetails matchingUser=user
+		
+		UserDetailsManager userMg=new myUserDetailsManager();
+		userMg.changePassword(input.getOldPassword(), input.getNewPassword());
+		
+		
+		return result;
+		
+		
+	}
+	@RequestMapping(value="/log_0001",method=RequestMethod.GET)
+	public ModelAndView log0001(){
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("account_setting");
+		return mv;
+	}
+	 
+	
+	/*
+	
+	@RequestMapping(value="customer_information")
+	public ModelAndView customerInformation(){
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("customer_information");
+		mv.addObject("page_id","customer_information");
+		return mv;
+	}
+
+	
+	 * @RequestMapping(value="/addUser", method=RequestMethod.POST) public
+	 * String addUser(@ModelAttribute MfiUserDto user){ userImp=new
+	 * MfiUserDaoImp(); userImp.insertUser(user); return "redirect:/"; }
+	 
+	
+
+
+	@RequestMapping(value = "/new_co", method = RequestMethod.GET)
+	public String newCo() {
+
+		return "new_co";
+	}
+
+	@RequestMapping(value = "/loan_agreement", method = RequestMethod.GET)
+	public String loanAgreement() {
+
+		return "loan_agreement";
+	}*/
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/customer_information.jsp
@@ -0,0 +1,151 @@
+
+<%@include file="include/_head.jsp"%>
+
+<%-- <%
+	HttpSession ses = request.getSession(false);
+	ses.setAttribute("id", "9");
+	ses.setAttribute("brand", "bb");
+%> --%>
+
+<body>
+
+	<div id="wrapper">
+		<!-- Navigation -->
+		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+			<!-- Fixed Up Navigation -->
+			<%@include file="include/_fixed_up_nav.jsp"%>
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<%@include file="include/_side_nav.jsp"%>
+			</div>
+
+		</nav>
+		<!-- End Navigation -->
+
+		<div id="page-wrapper">
+
+			<div class="container-fluid">
+
+				<!-- Page Heading -->
+				<div class="row">
+
+					<div class="col-lg-12 col-md-12 col-sm-12">
+
+						<h2 class="page-header">
+
+							<span></span>Customer Infomation
+							<!-- <button style="float: right;" type="button" class="btn btn-info" data-toggle="modal"
+							data-target="#myModal">Add Customer</button> -->
+						</h2>
+
+					</div>
+				</div>
+				<!--//Page Heading -->
+				<!-- list-->
+				<div class="row">
+					<div class="col-md-12 col-lg-12">
+						<!-- panel top-->
+						<div class="panel panel-default">
+							<div class="panel panel-heading">
+								<div class="row">
+									<div class="col-md-6">
+										<span class="col-md-8 col-sm-8" style="padding: 0px"> <select
+											class="form-control" id="co_info">
+												<option value="">Display All</option>
+
+										</select>
+										</span>
+
+									</div>
+									<div class="col-md-6 col-sm-6 pull-right">
+
+										<div class="form-group input-group">
+											<input class="form-control" type="text" id="word"
+												placeholder="Search Customer By ID Card or Name"> <span
+												class="input-group-btn"><button
+													class="btn btn-default" type="button">
+													<i class="fa fa-search"></i>
+												</button></span>
+										</div>
+
+									</div>
+								</div>
+
+							</div>
+							<!-- panel body -->
+							<div class="panel panel-body">
+								<div class="table-responsive">
+									<table class="table table-hover table-striped">
+										<thead class="style2">
+											<tr class="active">
+												<th>ID</th>
+												<th>Name</th>
+												<th>Nickname</th>
+												<th>Sex</th>
+												<th>Phone</th>
+												<th>Address</th>
+												<th>Action</th>
+											</tr>
+										</thead>
+										<tbody id="tableCustomer">
+
+
+										</tbody>
+									</table>
+								</div>
+
+							</div>
+							<!-- //panel body -->
+
+						</div>
+
+
+						<!--//panel top -->
+
+					</div>
+
+				</div>
+				<!-- //list -->
+				<div class="row">
+					<div class="col-md-1" id="select_num">
+						<select class='form-control' id='record_num' style='width: 100%'>
+							<option value='10'>10</option>
+							<option value='15'>15</option>
+							<option value='20'>20</option>
+							<option value='25'>25</option>
+						</select>
+					</div>
+					<div class="col-md-11" style="text-align: center;">
+						<!-- <div class="col-md-6 col-md-offset-3" style="text-align: center"> -->
+						<ul class="pagination" id="paging" style="margin: 0 auto;">
+						</ul>
+						<!-- </div> -->
+					</div>
+
+				</div>
+
+			</div>
+			<!-- /.container-fluid -->
+
+		</div>
+		<!-- /#page-wrapper -->
+	</div>
+	<!-- /#wrapper -->
+	<%@include file="customer_detail.jsp"%>
+
+	<%@include file="include/_script.jsp"%>
+
+	<script type="text/javascript">
+		var BASE_URL = "${pageContext.request.contextPath}";
+		var id="<%=strCoId%>";
+		//var PAGE_ID="${page_id}";
+	</script>
+
+	<script
+		src="${pageContext.request.contextPath}/resources/js/customer.js"></script>
+	<script
+		src="${pageContext.request.contextPath}/resources/js/customer_officer.js"></script>
+
+</body>
+
+</html>
