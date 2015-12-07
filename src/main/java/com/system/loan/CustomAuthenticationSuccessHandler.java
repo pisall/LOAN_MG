d0feebffb55e61c0a9ca650a14fe033e4d517835
Prak Virak <prakvirak54@gmail.com>
2015-12-07 16:44:15 +0700

    fef

--- src/main/java/com/system/loan/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/system/loan/CustomAuthenticationSuccessHandler.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -26,34 +27,49 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
         //do some logic here if you want something to be done whenever
         //the user successfully logs in.
     	System.out.println(" login successful !!!!!!))");
-        HttpSession session = httpServletRequest.getSession();
-        User authUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String userName=authUser.getUsername();
-        CO_DAO_001_IMP co=new CO_DAO_001_IMP();
-        HashMap<String, Object> coInfo=co.findCobyUserId(userName);
-        String strFirstNm=coInfo.get("co_first_nm").toString();
-        String strLastNm=coInfo.get("co_last_nm").toString();
-        String strBooOtherEditProf=coInfo.get("other_edit_prof").toString();
-        
-        System.out.println("is edit="+strBooOtherEditProf);
-        boolean booOtherEditProf=false;
-        if (strBooOtherEditProf.length()!=0){
-        	if(strBooOtherEditProf.equals("true")|| strBooOtherEditProf.equals("1"));
-        	booOtherEditProf=true;
-        }
-        int coId=(int)coInfo.get("co_id");
-        USER_SESSION userSession=new USER_SESSION();
-        userSession.setCoId(coId);
-        userSession.setCoNm(strFirstNm+" "+ strLastNm);
-        userSession.setOther_edit_prof(booOtherEditProf);
-        session.setAttribute("USER_SESSION", userSession/*authUser.getUsername()*/);
-        session.setAttribute("authorities", authentication.getAuthorities());
+    	
+    	String strRole="";
+    	for(GrantedAuthority gauth:authentication.getAuthorities()){
+    		strRole=gauth.getAuthority();
+    	}
+    	
+    	if(strRole.equals("ROLE_SYS")){
+    		 httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+    		 httpServletResponse.sendRedirect("/sys/sys_form");
+    		
+    	}else{
+    		 HttpSession session = httpServletRequest.getSession();
+	        User authUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	        String userName=authUser.getUsername();
+	        CO_DAO_001_IMP co=new CO_DAO_001_IMP();
+	        HashMap<String, Object> coInfo=co.findCobyUserId(userName);
+	        String strFirstNm=coInfo.get("co_first_nm").toString();
+	        String strLastNm=coInfo.get("co_last_nm").toString();
+	        String strBooOtherEditProf=coInfo.get("other_edit_prof").toString();
+	        
+	        System.out.println("is edit="+strBooOtherEditProf);
+	        boolean booOtherEditProf=false;
+	        if (strBooOtherEditProf.length()!=0){
+	        	if(strBooOtherEditProf.equals("true")|| strBooOtherEditProf.equals("1"));
+	        	booOtherEditProf=true;
+	        }
+	        int coId=(int)coInfo.get("co_id");
+	        USER_SESSION userSession=new USER_SESSION();
+	        userSession.setCoId(coId);
+	        userSession.setCoNm(strFirstNm+" "+ strLastNm);
+	        userSession.setOther_edit_prof(booOtherEditProf);
+	        session.setAttribute("USER_SESSION", userSession/*authUser.getUsername()*/);
+	        session.setAttribute("authorities", authentication.getAuthorities());
 
-        //set our response to OK status
-        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+	        //set our response to OK status
+	       
 
-        //since we have created our custom success handler, its up to us to where
-        //we will redirect the user after successfully login
-        httpServletResponse.sendRedirect("home");
+	        //since we have created our custom success handler, its up to us to where
+	        //we will redirect the user after successfully login
+	        httpServletResponse.sendRedirect("home");
+    		
+    	}
+    	
+       
     }
 }
